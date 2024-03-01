d8bd1e7be302dbde8b8f9e8edfb615d96eb86344
willcoope <cooperwilliam92@gmail.com>
2024-03-01 15:33:19 +0000

    Setup Express demo

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-import http from "http";
+// import http from "http";
+
+// const PORT = 4000;
+
+// const HOST = "127.0.0.1";
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.method);
+//   console.log(req.url);
+//   if (req.method === "POST" && req.url === `/add`) {
+//     res.end(`POST request to ${req.url} received`);
+//   } else {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(
+//       JSON.stringify({
+//         data: "Hello World",
+//       })
+//     );
+//   }
+// });
+
+// server.listen(PORT, HOST, () =>
+//   console.log(`Server listening on http://${HOST}:${PORT}`)
+// );
+
+import express from "express";
+const app = express();
 
 const PORT = 4000;
 
-const HOST = "127.0.0.1";
+app.get("/", (req, res) => res.end(`Hello from a  ${req.method} request`));
 
-const server = http.createServer((req, res) => {
-  console.log(req.method);
-  console.log(req.url);
-  if (req.method === "POST" && req.url === `/add`) {
-    res.end(`POST request to ${req.url} received`);
-  } else {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Hello World",
-      })
-    );
-  }
-});
+app.post("/", (req, res) => res.end(`Hello from a  ${req.method} request`));
 
-server.listen(PORT, HOST, () =>
-  console.log(`Server listening on http://${HOST}:${PORT}`)
-);
+app.post("/add", (req, res) => res.end(`Hello from a  ${req.method} request to ${req.url}`));
+
+const server = app.listen(PORT, () => {
+  const SERVERHOST = server.address().address;
+  const SERVERPORT = server.address().port;
+  console.log(`Server listening on http://${SERVERHOST}:${SERVERPORT}`);
+})
