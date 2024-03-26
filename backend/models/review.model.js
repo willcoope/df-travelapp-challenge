df8223dd858e03bcd664a1be8fb6224ea072a594
willcoope <cooperwilliam92@gmail.com>
2024-03-26 17:54:02 +0000

    Add Review model to backend

---
+++ backend/models/review.model.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    rating: {type: Number, required: true},
+    review: {type: String, required: true}
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;
